5681475e82e877e0a8949432835bc7eb6d79ac7d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-07-16 17:13:22 -0500

    Test opt feature

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 from ctypes import *
+from llvmlite import binding as llvm
 
 hlc = CDLL('libHLC.so')
-hlc.libHLC_Initialize()
-hlc.libHLC_Finalize()
+hlc.HLC_Initialize()
+
+with open("test_input.ll") as fin:
+    modtext = fin.read()
+
+# print(modtext)
+
+out = c_char_p(0)
+buf = create_string_buffer(modtext.encode("ascii"))
+
+print(hlc.HLC_Optimize(buf, byref(out)))
+
+print(out.value.decode("ascii"))
+
+hlc.HLC_Finalize()
+
+print("ok")
